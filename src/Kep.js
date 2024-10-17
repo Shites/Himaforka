79dfdef6390d26258f48bff8b6b4f28d80d36114
johndoe <johndoe@email.com>
2024-10-17 14:17:26 +0700

    y

--- src/Kep.js
+++ src/Kep.js
@@ -53,28 +53,13 @@ function reveal(){
 }
 
 
-var $content = $('.menu-content');
-
-function showContent(type) {
-  $content.hide().filter('.' + type).show();
-  $content.filter(':visible').fadeOut(0, function() 
-  {
-  $content.filter('.' + type).fadeIn(600);
-  });
-}
-
-$('.menu').on('click', '.menu-btn', function(e) {
-  showContent(e.currentTarget.hash.slice(1));
-  e.preventDefault();
-}); 
-
-// show 'about' content only on page load (if you want)
-showContent('pengurus');
-
-
 var $contentdb = $('.menu-content-db');
 
 function showContentdb(type) {
+    $contentdb.filter(':visible').fadeOut(0, function() 
+  {
+  $contentdb.filter('.' + type).fadeIn(600);
+  });
   $contentdb.hide().filter('.' + type).show();
 }
 
@@ -85,51 +70,10 @@ $('.menu-db').on('click', '.menu-btn-db', function(e) {
 }); 
 
 // show 'about' content only on page load (if you want)
-showContentdb('about');
-
-
-var $contentks = $('.menu-content-ks');
-
-function showContentks(type) {
-  $contentks.hide().filter('.' + type).show();
-  $contentks.filter(':visible').fadeOut(0, function() 
-  {
-  $contentks.filter('.' + type).fadeIn(600);
-  });
-}
-
-$('.menu-ks').on('click', '.menu-btn-ks', function(e) {
-  showContentks(e.currentTarget.hash.slice(1));
-  e.preventDefault();
-  
-}); 
-
-// show 'about' content only on page load (if you want)
-showContentks('ksp');
+showContentdb('ph');
 
 
 
-// Add active class to the current button (highlight it)
-var header = document.getElementById("kgt-nav");
-var btns = header.getElementsByClassName("agt-btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("stay");
-  current[0].className = current[0].className.replace(" stay", "");
-  this.className += " stay";
-  });
-}
-
-// Add active class to the current button (highlight it)
-var header = document.getElementById("ks-nav");
-var btns = header.getElementsByClassName("ks-btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("actv");
-  current[0].className = current[0].className.replace(" actv", "");
-  this.className += " actv";
-  });
-}
 
 // Add active class to the current button (highlight it)
 var header = document.getElementById("kp-nav");
