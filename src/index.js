c0bc3562c4ef13f6f9f24659be5cf69534e42d57
johndoe <johndoe@email.com>
2024-10-17 14:10:11 +0700

    sk sk klen aj

--- src/index.js
+++ src/index.js
@@ -32,6 +32,92 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+
+  for(var i = 0; i < reveals.length; i++){
+
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+
+var $content = $('.menu-content');
+
+function showContent(type) {
+  $content.hide().filter('.' + type).show();
+  $content.filter(':visible').fadeOut(0, function() 
+  {
+  $content.filter('.' + type).fadeIn(600);
+  });
+}
+
+$('.menu').on('click', '.menu-btn', function(e) {
+  showContent(e.currentTarget.hash.slice(1));
+  e.preventDefault();
+}); 
+
+// show 'about' content only on page load (if you want)
+showContent('pengurus');
+
+
+var $contentdb = $('.menu-content-db');
+
+function showContentdb(type) {
+  $contentdb.hide().filter('.' + type).show();
+}
+
+$('.menu-db').on('click', '.menu-btn-db', function(e) {
+  showContentdb(e.currentTarget.hash.slice(1));
+  e.preventDefault();
+  
+}); 
+
+// show 'about' content only on page load (if you want)
+showContentdb('about');
+
+
+var $contentks = $('.menu-content-ks');
+
+function showContentks(type) {
+  $contentks.hide().filter('.' + type).show();
+  $contentks.filter(':visible').fadeOut(0, function() 
+  {
+  $contentks.filter('.' + type).fadeIn(600);
+  });
+}
+
+$('.menu-ks').on('click', '.menu-btn-ks', function(e) {
+  showContentks(e.currentTarget.hash.slice(1));
+  e.preventDefault();
+  
+}); 
+
+// show 'about' content only on page load (if you want)
+showContentks('ksp');
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("kp-nav");
+var btns = header.getElementsByClassName("kp-btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("actv");
+  current[0].className = current[0].className.replace(" actv", "");
+  this.className += " actv";
+  });
+}
+
 // Add active class to the current button (highlight it)
 var header = document.getElementById("kgt-nav");
 var btns = header.getElementsByClassName("agt-btn");
@@ -56,63 +142,12 @@ for (var i = 0; i < btns.length; i++) {
 
 
 
-var $content = $('.menu-content');
-
-function showContent(type) {
-  $content.hide().filter('.' + type).show();
-  $content.filter(':visible').fadeOut(0, function() 
-  {
-  $content.filter('.' + type).fadeIn(600);
-  });
-}
-
-$('.menu').on('click', '.menu-btn', function(e) {
-  showContent(e.currentTarget.hash.slice(1));
-  e.preventDefault();
-}); 
-
-// show 'about' content only on page load (if you want)
-showContent('pengurus');
-
-var $contentks = $('.menu-content-ks');
-
-function showContentks(type) {
-  $contentks.hide().filter('.' + type).show();
-  $contentks.filter(':visible').fadeOut(0, function() 
-  {
-  $contentks.filter('.' + type).fadeIn(600);
-  });
-}
-
-$('.menu-ks').on('click', '.menu-btn-ks', function(e) {
-  showContentks(e.currentTarget.hash.slice(1));
-  e.preventDefault();
-  
-}); 
-
-// show 'about' content only on page load (if you want)
-showContentks('ksp');
 
 
 
 
-window.addEventListener('scroll', reveal);
 
-function reveal(){
-  var reveals = document.querySelectorAll('.reveal');
 
-  for(var i = 0; i < reveals.length; i++){
 
-    var windowheight = window.innerHeight;
-    var revealtop = reveals[i].getBoundingClientRect().top;
-    var revealpoint = 150;
 
-    if(revealtop < windowheight - revealpoint){
-      reveals[i].classList.add('active');
-    }
-    else{
-      reveals[i].classList.remove('active');
-    }
-  }
-}
 
